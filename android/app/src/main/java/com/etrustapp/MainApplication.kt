9b6a6f79e451a174d220fcee1249963bfb569cde
Tech Engineer <techengineer2026@gmail.com>
2026-02-04 15:06:00 +0530

    done

--- android/app/src/main/java/com/etrustapp/MainApplication.kt
+++ android/app/src/main/java/com/etrustapp/MainApplication.kt
@@ -1,39 +1,38 @@
 package com.etrustapp
 
 import android.app.Application
+import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
-  private val mReactNativeHost: ReactNativeHost =
-    object : DefaultReactNativeHost(this) {
-      override fun getUseDeveloperSupport(): Boolean {
-        return BuildConfig.DEBUG
+  override val reactNativeHost: ReactNativeHost =
+      object : DefaultReactNativeHost(this) {
+        override fun getPackages(): List<ReactPackage> =
+            PackageList(this).packages.apply {
+              // Packages that cannot be autolinked yet can be added manually here, for example:
+              // add(MyReactNativePackage())
+            }
+
+        override fun getJSMainModuleName(): String = "index"
+
+        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
       }
 
-      override fun getPackages(): List<ReactPackage> {
-        return com.facebook.react.PackageList(this).packages
-      }
-
-      override fun getJSMainModuleName(): String {
-        return "index"
-      }
-    }
-
-  override fun getReactNativeHost(): ReactNativeHost {
-    return mReactNativeHost
-  }
+  override val reactHost: ReactHost
+    get() = getDefaultReactHost(applicationContext, reactNativeHost)
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, false)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      DefaultNewArchitectureEntryPoint.load()
-    }
+    loadReactNative(this)
   }
 }
